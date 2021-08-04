8977085a32d735ab9f713c9317eb1d5250ce976e
Saif-K-Saeed <saif.k.saeed@gmail.com>
2021-08-04 11:45:11 +0300

    .

--- js/app.js
+++ js/app.js
@@ -5,154 +5,155 @@ let userName = prompt('Welcome to my page can you tell me your name');
 alert('Welcome again (' + userName + ') to my page I will ask you some questions about my self and you will answer by yes or no');
 
 
-function ques1() {
-  let myName = prompt('My name is saif?');
-  if (myName.toLowerCase() === 'yes' || myName.toLowerCase() === 'y') {
-    alert('This is a good starting');
-    console.log('First question:Your answer is true');
-    score++;
-  } else if (myName.toLowerCase() === 'no' || myName.toLowerCase() === 'n') {
-    alert('sorry this is not true');
-    console.log('First question:Your answer is false');
-  }
-  else {
-    alert('sorry you should answer by yes or no');
-    console.log('First question:You should pick yes or no');
-  }
+// function ques1() {
+let myName = prompt('My name is saif?');
+if (myName.toLowerCase() === 'yes' || myName.toLowerCase() === 'y') {
+  alert('This is a good starting');
+  console.log('First question:Your answer is true');
+  score++;
+} else if (myName.toLowerCase() === 'no' || myName.toLowerCase() === 'n') {
+  alert('sorry this is not true');
+  console.log('First question:Your answer is false');
 }
-ques1();
-
-
-function ques2() {
-  let favSport = prompt('My favorite sport is football?');
-  if (favSport.toLowerCase() === 'yes' || favSport.toLowerCase() === 'y') {
-    alert('sorry I do not like it');
-    console.log('Second question:Your answer is false');
-  } else if (favSport.toLowerCase() === 'no' || favSport.toLowerCase() === 'n') {
-    alert('You are amazing keep on it');
-    console.log('Second question:Your answer is true');
-    score++;
-  }
-  else {
-    alert('sorry you should answer by yes or no');
-    console.log('Second question:You should pick yes or no');
-  }
+else {
+  alert('sorry you should answer by yes or no');
+  console.log('First question:You should pick yes or no');
 }
-ques2();
+// }
+// ques1();
 
 
-function ques3() {
-  let favMeal = prompt('My favorite meal is mansaf?');
-  if (favMeal.toLowerCase() === 'yes' || favMeal.toLowerCase() === 'y') {
-    alert('yes this is true I fall in love with mansaf ');
-    console.log('Third question:Your answer is true');
-    score++;
-  } else if (favMeal.toLowerCase() === 'no' || favMeal.toLowerCase() === 'n') {
-    alert('I am so sorry I love mansaf');
-    console.log('Third question:Your answer is false');
-  }
-  else {
-    alert('sorry you should answer by yes or no');
-    console.log('Third question:You should pick yes or no');
-  }
+// function ques2() {
+let favSport = prompt('My favorite sport is football?');
+if (favSport.toLowerCase() === 'yes' || favSport.toLowerCase() === 'y') {
+  alert('sorry I do not like it');
+  console.log('Second question:Your answer is false');
+} else if (favSport.toLowerCase() === 'no' || favSport.toLowerCase() === 'n') {
+  alert('You are amazing keep on it');
+  console.log('Second question:Your answer is true');
+  score++;
 }
-ques3();
-
-
-
-
-
-function ques4() {
-  let favMeal = prompt('Am I 32 years old ?');
-  if (favMeal.toLowerCase() === 'yes' || favMeal.toLowerCase() === 'y') {
-    alert('yes this is true I am 32 years old  ');
-    console.log('Third question:Your answer is true');
-    score++;
-  } else if (favMeal.toLowerCase() === 'no' || favMeal.toLowerCase() === 'n') {
-    alert('I am so sorry I am 32 years old ');
-    console.log('Third question:Your answer is false');
-  }
-  else {
-    alert('sorry you should answer by yes or no');
-    console.log('Third question:You should pick yes or no');
-  }
-
-
+else {
+  alert('sorry you should answer by yes or no');
+  console.log('Second question:You should pick yes or no');
 }
-ques4();
+// }
+// ques2();
+
+
+// function ques3() {
+let favMeal = prompt('My favorite meal is mansaf?');
+if (favMeal.toLowerCase() === 'yes' || favMeal.toLowerCase() === 'y') {
+  alert('yes this is true I fall in love with mansaf ');
+  console.log('Third question:Your answer is true');
+  score++;
+} else if (favMeal.toLowerCase() === 'no' || favMeal.toLowerCase() === 'n') {
+  alert('I am so sorry I love mansaf');
+  console.log('Third question:Your answer is false');
+}
+else {
+  alert('sorry you should answer by yes or no');
+  console.log('Third question:You should pick yes or no');
+}
+// }
+// ques3();
+
+
+
+
+
+// function ques4() {
+let myAge = prompt('Am I 32 years old ?');
+if (myAge.toLowerCase() === 'yes' || myAge.toLowerCase() === 'y') {
+  alert('yes this is true I am 32 years old  ');
+  console.log('Third question:Your answer is true');
+  score++;
+} else if (myAge.toLowerCase() === 'no' ||myAge.toLowerCase() === 'n') {
+  alert('I am so sorry I am 32 years old ');
+  console.log('Third question:Your answer is false');
+}
+else {
+  alert('sorry you should answer by yes or no');
+  console.log('Third question:You should pick yes or no');
+}
+
+
+// }
+// ques4();
 
 //Third question
-function ques5() {
-  let favMeal = prompt('Am I from Jordan?');
-  if (favMeal.toLowerCase() === 'yes' || favMeal.toLowerCase() === 'y') {
-    alert('yes this is true I am from Jordan');
-    console.log('Third question:Your answer is true');
+// function ques5() {
+let myCountry = prompt('Am I from Jordan?');
+if (myCountry.toLowerCase() === 'yes' || myCountry.toLowerCase() === 'y') {
+  alert('yes this is true I am from Jordan');
+  console.log('Third question:Your answer is true');
+  score++;
+} else if (myCountry.toLowerCase() === 'no' || myCountry.toLowerCase() === 'n') {
+  alert('I am so sorry I am from Jordan');
+  console.log('Third question:Your answer is false');
+}
+else {
+  alert('sorry you should answer by yes or no');
+  console.log('Third question:You should pick yes or no');
+}
+// }
+// ques5();
+
+
+
+// function ques6() {
+let correctNumber = 6;
+for (let i = 1; i <= 4; i++) {
+  let guessNum = parseInt(prompt('Can you guess my favorite number. Please type a number.'));
+  if (guessNum === correctNumber) {
+    alert('That\'s rigth you are amazing');
+    console.log('fouth question:Your answer is true');
     score++;
-  } else if (favMeal.toLowerCase() === 'no' || favMeal.toLowerCase() === 'n') {
-    alert('I am so sorry I am from Jordan');
-    console.log('Third question:Your answer is false');
+    break;
+  }
+  else if (guessNum > correctNumber) {
+    alert('sorry, It\'s too high');
+    console.log('fouth question:Your answer is false');
+  }
+  else if (guessNum < correctNumber) {
+    alert('sorry, It\'s too low');
+    console.log('fouth question:Your answer is false ');
   }
   else {
-    alert('sorry you should answer by yes or no');
-    console.log('Third question:You should pick yes or no');
+    console.log('fouth question:Your answer is false');
   }
 }
-ques5();
+alert(`My favorate number is ${correctNumber}`);
+
+// }
+// ques6();
 
 
-
-function ques6() {
-  for (let i = 0; i < 4; i++) {
-    let guessNum = parseInt(prompt('Can you guess my favorite number. Please type a number.'));
-    if (guessNum === 6) {
-      alert('That\'s rigth you are amazing');
-      console.log('fouth question:Your answer is true');
+// function ques7() {
+let favFruit = ['kiwi', 'Peache', 'apple', 'banana', 'mango'];
+let flag = false;
+for (let i = 1; i <= 6; i++) {
+  let guessFruit = prompt('Can you guess my favorite fruit. Please type a name of fruit.');
+  guessFruit = guessFruit.toLowerCase();
+  for (let j = 0; j < favFruit.length; j++) {
+    if (guessFruit === favFruit[j]) {
+      alert('That\'s true');
+      console.log('fifth question:Your answer is true');
       score++;
-      break;
-    }
-    else if (guessNum > 6) {
-      alert('sorry, It\'s too high');
-      console.log('fouth question:Your answer is false');
-    }
-    else if (guessNum < 6) {
-      alert('sorry, It\'s too low');
-      console.log('fouth question:Your answer is false');
+      flag =true;
     }
     else {
-      console.log('fouth question:Your answer is false');
+      console.log('fifth question:Your answer is false');
     }
   }
-  alert('My favorate number is 6');
-
-}
-ques6();
-
-
-function ques7() {
-  let favFruit = ['kiwi', 'Peache', 'apple', 'banana', 'mango'];
-  let flag = false;
-  for (let j = 0; j < 6; j++) {
-    let guessFruit = prompt('Can you guess my favorite fruit. Please type a name of fruit.');
-    guessFruit = guessFruit.toLowerCase();
-    for (let r = 0; r < favFruit.length; r++) {
-      if (guessFruit === favFruit[r]) {
-        alert('That\'s true');
-        console.log('fifth question:Your answer is true');
-        score++;
-        flag =true;
-      }
-      else {
-        console.log('fifth question:Your answer is false');
-      }
-    }
-    if(flag){
-      break;
-    }
-
+  if(flag){
+    break;
   }
-  alert('My favorate fruit are [kiwi, Peache, apple,banana, mango]');
+
 }
-ques7();
+alert('My favorate fruit are [kiwi, Peache, apple,banana, mango]');
+// }
+// ques7();
 
 alert(`Thank you ${userName} Your score is = ${score}/7 `);
 console.log('your score =', score);
